8cd83efd54cb8c93b50a0496e349191e652ffd18
venkyroyal <117074116+venkeyroyal@users.noreply.github.com>
2026-04-05 10:29:32 +0530

    Implement Jenkins pipeline in app.py
    Added a Jenkins pipeline for cloning repo and running Python script.

--- myproject/app.py
+++ myproject/app.py
@@ -1,1 +1,27 @@
-print("My Jenkins Project Running")
+pipeline {
+    agent any
+
+    stages {
+        stage('Clone') {
+            steps {
+                // Clone GitHub repo main branch
+                git branch: 'main', url: 'https://github.com/venkeyroyal/leaarning.git'
+            }
+        }
+
+        stage('Check files') {
+            steps {
+                echo 'Checking workspace files...'
+                bat 'dir'
+            }
+        }
+
+        stage('Run Python') {
+            steps {
+                echo 'Running Python project...'
+                // Run existing Python file Hello_world.py
+                bat 'C:\\Users\\VENKY\\AppData\\Local\\Programs\\Python\\Python312\\python.exe Hello_world.py'
+            }
+        }
+    }
+}
